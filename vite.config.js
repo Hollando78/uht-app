bddbafde133364860bb3977834fb9dda7bccb7b7
Hollando78 <130513710+Hollando78@users.noreply.github.com>
2025-04-28 20:26:49 +0100

    changed

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/uht-app/',  // 👈 Very important for GitHub Pages!!
+  base: '/uht-app/',  // 👈 Very important for GitHub Pages
 });
